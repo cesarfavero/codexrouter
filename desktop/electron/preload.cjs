b640af8a46665365c925d6e02fb1d0c1a191a4e5
Cesar <72269544+cesarfavero@users.noreply.github.com>
2026-09-13 16:57:18 -0300

    feat: expose secure desktop IPC bridge

---
+++ desktop/electron/preload.cjs
@@ -0,0 +1,29 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+function invoke(channel, ...args) {
+  return ipcRenderer.invoke(channel, ...args);
+}
+
+function onEvent(callback) {
+  const listener = (_event, payload) => callback(payload);
+  ipcRenderer.on('codexrouter:event', listener);
+  return () => ipcRenderer.removeListener('codexrouter:event', listener);
+}
+
+contextBridge.exposeInMainWorld('codexRouter', {
+  snapshot: () => invoke('codexrouter:snapshot'),
+  addAccount: label => invoke('codexrouter:account:add', label),
+  reauthenticateAccount: accountId => invoke('codexrouter:account:reauth', accountId),
+  removeAccount: accountId => invoke('codexrouter:account:remove', accountId),
+  setDefaultAccount: accountId => invoke('codexrouter:account:default', accountId),
+  syncCatalog: () => invoke('codexrouter:catalog:sync'),
+  install: () => invoke('codexrouter:integration:install'),
+  uninstall: () => invoke('codexrouter:integration:uninstall'),
+  startRouter: () => invoke('codexrouter:runtime:start'),
+  stopRouter: () => invoke('codexrouter:runtime:stop'),
+  openCodex: () => invoke('codexrouter:open-codex'),
+  setAutostart: enabled => invoke('codexrouter:autostart:set', enabled),
+  openExternal: url => invoke('codexrouter:open-external', url),
+  revealData: () => invoke('codexrouter:reveal-data'),
+  onEvent,
+});
